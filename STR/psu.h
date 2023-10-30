e5bc2b2dac01d4c709a4b37e03b36c7931481431
7244 <65ad4d@gmail.com>
2023-10-30 16:03:11 +0000

    up

--- STR/psu.h
+++ STR/psu.h
@@ -2,56 +2,56 @@
 
 #include _WITCH_PATH(STR/common/common.h)
 
-static uint32_t STR_psu32(const uint8_t *str, uintptr_t size){
+static uint32_t STR_psu32(const void *str, uintptr_t size){
   uint32_t res = 0;
   for(uintptr_t i = 0; i < size; i++)
-    res = res * 10 + str[i] - '0';
+    res = res * 10 + ((uint8_t *)str)[i] - '0';
   return res;
 }
-static uint64_t STR_psu64(const uint8_t *str, uintptr_t size){
+static uint64_t STR_psu64(const void *str, uintptr_t size){
   uint64_t res = 0;
   for(uintptr_t i = 0; i < size; i++)
-    res = res * 10 + str[i] - '0';
+    res = res * 10 + ((uint8_t *)str)[i] - '0';
   return res;
 }
-static uintptr_t STR_psu(const uint8_t *str, uintptr_t size){
+static uintptr_t STR_psu(const void *str, uintptr_t size){
   return CONCAT(STR_psu, SYSTEM_BIT)(str, size);
 }
 
-static uint32_t STR_psu32_iguess(const uint8_t *str, uintptr_t *i){
+static uint32_t STR_psu32_iguess(const void *str, uintptr_t *i){
   uint32_t res = 0;
-  for(; STR_ischar_digit(str[*i]); (*i)++)
-    res = res * 10 + str[*i] - '0';
+  for(; STR_ischar_digit(((uint8_t *)str)[*i]); (*i)++)
+    res = res * 10 + ((uint8_t *)str)[*i] - '0';
   return res;
 }
-static uint64_t STR_psu64_iguess(const uint8_t *str, uintptr_t *i){
+static uint64_t STR_psu64_iguess(const void *str, uintptr_t *i){
   uint64_t res = 0;
-  for(; STR_ischar_digit(str[*i]); (*i)++)
-    res = res * 10 + str[*i] - '0';
+  for(; STR_ischar_digit(((uint8_t *)str)[*i]); (*i)++)
+    res = res * 10 + ((uint8_t *)str)[*i] - '0';
   return res;
 }
-static uintptr_t STR_psu_iguess(const uint8_t *str, uintptr_t *i){
+static uintptr_t STR_psu_iguess(const void *str, uintptr_t *i){
   return CONCAT(STR_psu, _iguess)(str, i);
 }
 
-static uint32_t STR_psu32_i(const uint8_t *str, uintptr_t *i, const uintptr_t size){
+static uint32_t STR_psu32_i(const void *str, uintptr_t *i, const uintptr_t size){
   uint32_t res = 0;
   for(; *i < size; (*i)++){
-    if(!STR_ischar_digit(str[*i]))
+    if(!STR_ischar_digit(((uint8_t *)str)[*i]))
       break;
-    res = res * 10 + str[*i] - '0';
+    res = res * 10 + ((uint8_t *)str)[*i] - '0';
   }
   return res;
 }
-static uint64_t STR_psu64_i(const uint8_t *str, uintptr_t *i, const uintptr_t size){
+static uint64_t STR_psu64_i(const void *str, uintptr_t *i, const uintptr_t size){
   uint64_t res = 0;
   for(; *i < size; (*i)++){
-    if(!STR_ischar_digit(str[*i]))
+    if(!STR_ischar_digit(((uint8_t *)str)[*i]))
       break;
-    res = res * 10 + str[*i] - '0';
+    res = res * 10 + ((uint8_t *)str)[*i] - '0';
   }
   return res;
 }
-static uintptr_t STR_psu_i(const uint8_t *str, uintptr_t *i, const uintptr_t size){
+static uintptr_t STR_psu_i(const void *str, uintptr_t *i, const uintptr_t size){
   return CONCAT3(STR_psu, SYSTEM_BIT, _i)(str, i, size);
 }
