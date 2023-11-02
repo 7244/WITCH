063bae848f035670b4ae131dfbed7c6e839c3e37
reinterpret-pointer-cast <joel.mieskonen@gmail.com>
2023-11-02 15:58:30 +0200

    up

--- PR/backend/windows/windows.h
+++ PR/backend/windows/windows.h
@@ -11,7 +11,7 @@
     #include <iostream>
     #include <stacktrace>
 
-    static void _PR_DumpTrace() {
+    static void _PR_DumpTrace_CPP() {
       #if __cplusplus >= 202004L
       std::stacktrace st;
       std::cout << st.current();
@@ -19,51 +19,50 @@
       std::cout << "failed to print stacktrace";
       #endif
     }
-  #else
+  #endif
     #include <stdio.h>
     #include <dbghelp.h>
 
-    #ifdef _MSC_VER
-      #pragma comment(lib, "Dbghelp.lib")
-    #endif
-
-    static void _PR_DumpTrace() {
-      uint16_t i;
-      uint16_t frames;
-      void *stack[0xff];
-      SYMBOL_INFO *symbol;
-      HANDLE process;
-
-      SymSetOptions(SYMOPT_LOAD_LINES | SYMOPT_DEFERRED_LOADS | SYMOPT_INCLUDE_32BIT_MODULES);
-
-      process = GetCurrentProcess();
-
-      if (!SymInitialize(process, NULL, TRUE)) {
-        int err = GetLastError();
-        printf("[_PR_DumpTrace] SymInitialize failed %d", err);
-      }
-
-      frames = CaptureStackBackTrace(0, 0xff, stack, NULL);
-      symbol = (SYMBOL_INFO*)calloc(sizeof(SYMBOL_INFO) + 1024 * sizeof(uint8_t), 1);
-      symbol->MaxNameLen = 1023;
-      symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-
-      for (i = 0; i < frames; i++) {
-        SymFromAddr(process, (DWORD64)(stack[i]), 0, symbol);
-        DWORD Displacement;
-        IMAGEHLP_LINE64 Line;
-        Line.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
-        if (SymGetLineFromAddr64(process, (DWORD64)(stack[i]), &Displacement, &Line)) {
-          printf("%i: %s:%u\n", frames - i - 1, symbol->Name, Line.LineNumber);
-        }
-        else {
-          printf("%i: %s:0x%x\n", frames - i - 1, symbol->Name, symbol->Address);
-        }
-      }
-
-      free(symbol);
-    }
+  #ifdef _MSC_VER
+    #pragma comment(lib, "Dbghelp.lib")
   #endif
+
+  static void _PR_DumpTrace() {
+    uint16_t i;
+    uint16_t frames;
+    void *stack[0xff];
+    SYMBOL_INFO *symbol;
+    HANDLE process;
+
+    SymSetOptions(SYMOPT_LOAD_LINES | SYMOPT_DEFERRED_LOADS | SYMOPT_INCLUDE_32BIT_MODULES);
+
+    process = GetCurrentProcess();
+
+    if (!SymInitialize(process, NULL, TRUE)) {
+      int err = GetLastError();
+      printf("[_PR_DumpTrace] SymInitialize failed %d", err);
+    }
+
+    frames = CaptureStackBackTrace(0, 0xff, stack, NULL);
+    symbol = (SYMBOL_INFO*)calloc(sizeof(SYMBOL_INFO) + 1024 * sizeof(uint8_t), 1);
+    symbol->MaxNameLen = 1023;
+    symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+
+    for (i = 0; i < frames; i++) {
+      SymFromAddr(process, (DWORD64)(stack[i]), 0, symbol);
+      DWORD Displacement;
+      IMAGEHLP_LINE64 Line;
+      Line.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
+      if (SymGetLineFromAddr64(process, (DWORD64)(stack[i]), &Displacement, &Line)) {
+        printf("%i: %s:%u\n", frames - i - 1, symbol->Name, Line.LineNumber);
+      }
+      else {
+        printf("%i: %s:0x%x\n", frames - i - 1, symbol->Name, symbol->Address);
+      }
+    }
+
+    free(symbol);
+  }
 
   static void _PR_SignalCatcher(int signal) {
     if (signal == SIGINT) {
@@ -87,8 +86,17 @@ static void PR_exit(uint32_t num){
 #if _PR_set_abort_print
   static void PR_abort(void) {
     printf("[PR] PR_abort() is called\n");
+    // if bigger or equal to c++23 for std::stacktrace
+  #if WL_CPP >= 202302L
+    _PR_DumpTrace_CPP();
+  #else
     _PR_DumpTrace();
+  #endif
+  #if defined(WL_CPP)
+    throw std::exception("[PR] PR_abort() is called");
+  #else
     PR_exit(1);
+  #endif
   }
 #else
   static void PR_abort(void) {
