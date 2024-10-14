38452a26ce5c54bf341191d7d01e4084c08d4d98
7244 <65ad4d@gmail.com>
2024-10-14 01:47:17 +0000

    platform update

--- internal/COMPILER.h
+++ internal/COMPILER.h
@@ -142,7 +142,7 @@
       typedef unsigned char uint8_t;
       typedef unsigned short uint16_t;
       typedef unsigned int uint32_t;
-      typedef uintptr_t uint64_t;
+      typedef unsigned long long uint64_t;
     #endif
   #elif SYSTEM_BIT == 32
     typedef signed long sintptr_t;
@@ -154,7 +154,7 @@
       typedef unsigned long uintptr_t;
       typedef unsigned char uint8_t;
       typedef unsigned short uint16_t;
-      typedef uintptr_t uint32_t;
+      typedef unsigned int uint32_t;
       typedef unsigned long long uint64_t;
     #endif
   #else
