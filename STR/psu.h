cc2dc0708a3c95c1755cd1de718fd3fc5d5dd0eb
7244 <65ad4d@gmail.com>
2025-02-12 02:06:11 +0000

    STR psu up

--- STR/psu.h
+++ STR/psu.h
@@ -18,20 +18,42 @@ static uintptr_t STR_psu(const void *str, uintptr_t size){
   return CONCAT(STR_psu, SYSTEM_BIT)(str, size);
 }
 
-static uint32_t STR_psu32_iguess(const void *str, uintptr_t *i){
-  uint32_t res = 0;
-  for(; STR_ischar_digit(((uint8_t *)str)[*i]); (*i)++)
-    res = res * 10 + ((uint8_t *)str)[*i] - '0';
-  return res;
-}
-static uint64_t STR_psu64_iguess(const void *str, uintptr_t *i){
-  uint64_t res = 0;
-  for(; STR_ischar_digit(((uint8_t *)str)[*i]); (*i)++)
-    res = res * 10 + ((uint8_t *)str)[*i] - '0';
-  return res;
-}
-static uintptr_t STR_psu_iguess(const void *str, uintptr_t *i){
+#define STR_def_psu_iguess(bit) \
+  static uint ## bit ## _t STR_psu ## bit ## _iguess(const void *str, uintptr_t *i){ \
+    uint ## bit ## _t res = 0; \
+    for(; STR_ischar_digit(((uint8_t *)str)[*i]); (*i)++){ \
+      res = res * 10 + ((uint8_t *)str)[*i] - '0'; \
+    } \
+    return res; \
+  }
+STR_def_psu_iguess(8)
+STR_def_psu_iguess(16)
+STR_def_psu_iguess(32)
+STR_def_psu_iguess(64)
+#undef STR_def_psu_iguess
+static __forceinline uintptr_t STR_psu_iguess(const void *str, uintptr_t *i){
   return CONCAT3(STR_psu, SYSTEM_BIT, _iguess)(str, i);
+}
+
+#define STR_def_psu_iguess_abort(bit) \
+  static uint ## bit ## _t STR_psu ## bit ##_iguess_abort(const void *str, uintptr_t *i){ \
+    uint ## bit ## _t res = 0; \
+    uintptr_t start = *i; \
+    for(; STR_ischar_digit(((uint8_t *)str)[*i]); (*i)++){ \
+      res = res * 10 + ((uint8_t *)str)[*i] - '0'; \
+    } \
+    if(start == *i){ \
+      __abort(); \
+    } \
+    return res; \
+  }
+STR_def_psu_iguess_abort(8)
+STR_def_psu_iguess_abort(16)
+STR_def_psu_iguess_abort(32)
+STR_def_psu_iguess_abort(64)
+#undef STR_def_psu_iguess_abort
+static __forceinline uintptr_t STR_psu_iguess_abort(const void *str, uintptr_t *i){
+  return CONCAT3(STR_psu, SYSTEM_BIT, _iguess_abort)(str, i);
 }
 
 static uint32_t STR_psu32_i(const void *str, uintptr_t *i, const uintptr_t size){
