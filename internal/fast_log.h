8a2232fb85a778410bd670e2234f4429ed9fe4f9
7244 <65ad4d@gmail.com>
2025-03-06 16:56:21 +0000

    fast_log.h fixes

--- internal/fast_log.h
+++ internal/fast_log.h
@@ -1,25 +1,25 @@
 #ifndef __fast_8log2
   #define __fast_8log2 __fast_8log2
   __forceinline static uint8_t __fast_8log2(uint8_t v){
-    return 31 - __builtin_clz(v);
+    return 31 - __clz32(v);
   }
 #endif
 #ifndef __fast_16log2
   #define __fast_16log2 __fast_16log2
   __forceinline static uint8_t __fast_16log2(uint16_t v){
-    return 31 - __builtin_clz(v) - 16;
+    return 31 - __clz32(v);
   }
 #endif
 #ifndef __fast_32log2
   #define __fast_32log2 __fast_32log2
   __forceinline static uint8_t __fast_32log2(uint32_t v){
-    return 31 - __builtin_clz(v);
+    return 31 - __clz(v);
   }
 #endif
 #ifndef __fast_64log2
   #define __fast_64log2 __fast_64log2
   __forceinline static uint8_t __fast_64log2(uint64_t v){
-    return 63 - __builtin_clzll(v);
+    return 63 - __clz64(v);
   }
 #endif
 #ifndef __fast_log2
