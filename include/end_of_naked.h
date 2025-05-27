be2c698b57ee75ce58e41e2e4577e8227dfdcc5a
6413 <56801084+6413@users.noreply.github.com>
2025-05-28 01:02:42 +0300

    Update end_of_naked.h

--- include/end_of_naked.h
+++ include/end_of_naked.h
@@ -1,4 +1,4 @@
-#if defined(__compiler_clang) && __clang_major__ >= 19 && __clang_major__ <= 19
+#if defined(__compiler_clang) && __clang_major__ >= 19 && __clang_major__ <= 20
   /* ~poolaki~ */
 #elif defined(__compiler_gcc)
   /* gcc generates undefined instruction without unreachable */
