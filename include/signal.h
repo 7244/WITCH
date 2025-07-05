8bfc51836935a8981afe0d720cf44add75b2fcf0
7244 <65ad4d@gmail.com>
2025-07-05 03:28:28 +0000

    signal.h up

--- include/signal.h
+++ include/signal.h
@@ -7,26 +7,7 @@
 #else
   /* TODO last time i tried this backend: it didnt work well */
   /* i guess i need to learn signals raw-system-call-wise */
-
-  #include _WITCH_PATH(include/syscall.h)
-  #include <asm/signal.h>
-  typedef void (*__sighandler_t)(int);
-  typedef __sighandler_t sighandler_t;
-  sighandler_t signal(sint32_t signum, sighandler_t handler){
-    struct sigaction src;
-    src.sa_handler = SIG_DFL;
-    src.sa_mask = 0;
-    src.sa_flags = 0;
-    src.sa_restorer = 0;
-    struct sigaction dst;
-    dst.sa_handler = handler;
-    dst.sa_mask = 0;
-    dst.sa_flags = 0;
-    dst.sa_restorer = 0;
-    syscall4(__NR_rt_sigaction, signum, (uintptr_t)&dst, (uintptr_t)&src, 8);
-    return 0;
-  }
-
+  
   typedef union WITCH_signal_sigval{
     int sival_int;
     void *sival_ptr;
@@ -42,23 +23,23 @@
     (__alignof__(void *) < sizeof(short) ? sizeof(short) : __alignof__(void *))
   union _WITCH_signal_sifields{
     struct{
-      __kernel_pid_t _pid;
-      __kernel_uid32_t _uid;
+      int _pid;
+      unsigned int _uid;
     }_kill;
     struct{
-      __kernel_timer_t _tid;
+      int _tid;
       int _overrun;
       WITCH_signal_sigval_t _sigval;
       int _sys_private;
     }_timer;
     struct{
-      __kernel_pid_t _pid;
-      __kernel_uid32_t _uid;
+      int _pid;
+      unsigned int _uid;
       WITCH_signal_sigval_t _sigval;
     }_rt;
     struct{
-      __kernel_pid_t _pid;
-      __kernel_uid32_t _uid;
+      int _pid;
+      unsigned int _uid;
       int _status;
       uint64_t _utime;
       uint64_t _stime;
@@ -76,12 +57,12 @@
         }_addr_bnd;
         struct{
           char _dummy_pkey[__ADDR_BND_PKEY_PAD];
-          __u32 _pkey;
+          uint32_t _pkey;
         }_addr_pkey;
         struct{
           unsigned long _data;
-          __u32 _type;
-          __u32 _flags;
+          uint32_t _type;
+          uint32_t _flags;
         }_perf;
       };
     }_sigfault;
@@ -119,4 +100,43 @@
       uint32_t _si_pad[_WITCH_PR_SI_MAX_SIZE / sizeof(uint32_t)];
     };
   }WITCH_signal_siginfo_t;
+
+  #include _WITCH_PATH(include/syscall.h)
+  #include <asm/signal.h>
+  typedef void (*__sighandler_t)(int);
+  typedef __sighandler_t sighandler_t;
+
+  #ifdef __i386__
+    struct WITCH_sigaction {
+	    union {
+	      __sighandler_t _sa_handler;
+	      void (*_sa_sigaction)(int, WITCH_signal_siginfo_t *, void *);
+	    } _u;
+	    unsigned long sa_mask;
+	    unsigned long sa_flags;
+	    void (*sa_restorer)(void);
+    };
+  #else
+    typedef struct{
+	    __sighandler_t sa_handler;
+	    unsigned long sa_flags;
+	    __sigrestore_t sa_restorer;
+	    unsigned long sa_mask;
+    }WITCH_signal_sigaction_t;
+  #endif
+
+  static sighandler_t signal(sint32_t signum, sighandler_t handler){
+    WITCH_signal_sigaction_t src;
+    src.sa_handler = SIG_DFL;
+    src.sa_mask = 0;
+    src.sa_flags = 0;
+    src.sa_restorer = 0;
+    WITCH_signal_sigaction_t dst;
+    dst.sa_handler = handler;
+    dst.sa_mask = 0;
+    dst.sa_flags = 0;
+    dst.sa_restorer = 0;
+    syscall4(__NR_rt_sigaction, signum, (uintptr_t)&dst, (uintptr_t)&src, 8);
+    return 0;
+  }
 #endif
