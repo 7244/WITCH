a213331af77476c1882cfeff2dd6b516ce197a5d
7244 <65ad4d@gmail.com>
2026-03-27 05:16:17 +0000

    _auto_generated.h up

--- internal/_auto_generated.h
+++ internal/_auto_generated.h
@@ -1,7 +1,7 @@
 #ifndef _PP_ARG_FIRST_OR_SECOND
-  #define _PP_ARG_FIRST_OR_SECOND_1(_1, ...) _1
+  #define _PP_ARG_FIRST_OR_SECOND_1(_1) _1
   #define _PP_ARG_FIRST_OR_SECOND_2(_1, _2) _2
-  #define _PP_ARG_FIRST_OR_SECOND(_1, ...) CONCAT(_PP_ARG_FIRST_OR_SECOND_, _PP_NARG(__VA_ARGS__))(_1, __VA_ARGS__)
+  #define _PP_ARG_FIRST_OR_SECOND(...) CONCAT(_PP_ARG_FIRST_OR_SECOND_, _PP_NARG(__VA_ARGS__))(__VA_ARGS__)
 #endif
 
 #if defined(__compiler_msvc)
